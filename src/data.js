0c2766e279bdc167a3b2a1ed905eefd8cbd64f88
adam robson <adamray312@gmail.com>
2023-03-15 21:47:33 -0700

    add data.js and browser router

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const writings = [
+  {
+    id: '1',
+    name: 'silence',
+    title: 'the realm the silence heard',
+    text: ['you are pacing instants, ', 'a rhythmic ethereal air, ', 'acute distraction draped upon, ', 'a far off dreamy layer. ', 'where eveything is happenstance, ', 'each face another place, ', 'take either way and let it pass, ', 'soon we call it fate. ', 'there\'s warmth in practice,', 'taking time - to learn to contemplate, ', 'in the midst of mostly noise, ', 'subconscious congregates. ', 'the realm that birthed the universe, ', 'a realm that sees your words, ', 'in this collage, ', 'our senses wait, ', 'in the realm the silence heard. ']
+  },
+  {
+    id: '2',
+    name: 'tuesday',
+    title: 'tuesday',
+    text: ['a fickle freckled fawn, ', 'treated dawn, ', 'with a yawn. ', 'as tears of newly wake, ', 'seeped through, ', 'her eyeballs drape. ', 'swaying back and forth, ', 'between shadows senses dreamed, ', 'she gleamed, ', 'disguising happenstance, ', 'with people that shed seen. ', 'the soundtrack was, ', 'their laughing rhythm, ', 'floating words like fiends, ', 'tickling her smile strings, ', 'a euphoric sleepy lean. ']
+  },
+  {
+    id: '3',
+    name: 'earth',
+    title: 'heaven on earth',
+    text: ['the static of the earth ', 'attracts the magnet of a mind. ', 'the fabric of the earth, ', 'informs the senses, ', 'of the lines. ', 'the will inside the earth, ', 'humbles each and every heir, ', 'the brilliance of the earth, ', 'arrives acutely, ', 'and can tear. ', 'the contents of the earth, ', 'include the nows that all have passed. ', 'the shadows on the earth, ', 'hide the light, ', 'that can\'t be grasped. ', 'the folks on top of earth, ', 'looking elsewhere for their souls, ', 'miss the part of earth, ', 'that harbors heaven and its holes']
+  },
+  {
+    id: '4',
+    name: 'preference',
+    title: 'preference',
+    text: ['from the earliest nudge of dawn, ', 'to the cathartic drawl of noon, ', 'the primary precious and pregnant portion of the day, ', 'is therapeutic at least, ', 'and often appears drenched, ', 'in the excitement of possibility, ', 'the time of day designated, ', 'for solitude reflection and thankful, ', 'for silence leisure and breakfast, ', 'is best enjoyed with open windows, ', 'and accompanied by music, ', 'occasionally shared with, ', 'dearest friends and sleepy lovers, ', 'cultivate reverence for, ', 'the introduction of today, ', 'and today will show you, ', 'the most profound and primordial, ', 'respect and encouragement', 'you will ever know.']
+  },
+  {
+    id: '5',
+    name: 'between',
+    title: 'in between',
+    text: ['while here i hear a tongue, ', 'feel the trees against the sun. ', 'in the breeze hear spirits breathe. ', 'the world weaves in gravity. ', 'where many can not taste, ', 'i find i fill up to my face. ', 'see the thoughts, ', 'just like the clouds, ', 'will soon reveal, ', 'and sooner shroud. ', 'where even knowing, ', 'becomes confusing, ', 'and soon enough, ', 'becomes amusing, ', 'pinch my skin, ', 'and i will budge. ', 'i am in between, ', 'a grin and nudge.']
+  },
+  {
+    id: '6',
+    name: 'water weight',
+    title: 'water weight',
+    text: ['a tiny earthen crease, ', 'irrigation and release, ', 'precious drip, ', 'a tiny leak, ', 'walking water, ', 'wears all weak, ', 'a gymnast in a gentle stream, ', 'soon to swallow everything']
+  },
+  {
+    id: 7,
+    name: 'planet earth',
+    title: 'planet earth',
+    text: ['almost all water', 'and within every dew drop', 'brids punctuate the days']
+  }];
