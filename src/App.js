0fe46d549d78ec5d121845b2df9d55f9d96b435f
adam robson <adamray312@gmail.com>
2023-03-15 22:49:17 -0700

    add the writing component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Arts from './components/Arts/Arts';
 import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
+import Writing from './components/Writing/Writing';
 import Error from './components/Error/Error';
 
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { writings } from './data.js';
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/about" element={ <About /> } />
         <Route path="/projects" element={ <Projects /> } />
-        <Route path="/arts" element={ <Arts /> } />
         <Route path="/contact" element={ <Contact /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/arts/:id" element={ <Writing writings={ writings } /> } />
+        <Route path="/arts" element={ <Arts /> } />
+        <Route path="/" element={ <Home /> } />
         <Route path="*" element={ <Error /> } />
       </Routes>
     </>
