85eb2e306f1fdc84f27039abe09c229be7f5501c
adam robson <adamray312@gmail.com>
2023-03-15 22:16:49 -0700

    add app with routes and elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Arts from './components/Arts/Arts';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Error from './components/Error/Error';
+
+import {
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/projects" element={ <Projects /> } />
+        <Route path="/arts" element={ <Arts /> } />
+        <Route path="/contact" element={ <Contact /> } />
+        <Route path="*" element={ <Error /> } />
+      </Routes>
+    </>
   );
 }
-
-export default App;
