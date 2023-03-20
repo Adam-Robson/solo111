cc8040466a1b49bc445b2f3cfdc52a402d59584b
adam robson <adamray312@gmail.com>
2023-03-20 16:33:13 -0700

    place links at bottom of page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
           <h3 className="text-center text-sm font-light tracking-wider subpixel-antialiased my-32 mx-8 md:text-lg">
             <i>creative software solutions designed using progressive programming methods</i>
           </h3>
-          <ul className="flex justify-evenly items-end min-w-full subpixel-antialiased">
+          <ul className="flex justify-evenly items-end min-w-full absolute bottom-0 mb-12 subpixel-antialiased">
             <li className="cursor-pointer"><a href="/about">about</a></li>
             <li className="cursor-pointer"><a href="/projects">projects</a></li>
             <li className="cursor-pointer"><a href="/arts">arts</a></li>
