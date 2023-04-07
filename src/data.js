4e1ab62c255a7a4d0d05c50d4bd8ba4404174b6d
adam robson <adamray312@gmail.com>
2023-04-07 10:47:29 -0700

    add writing

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const writings = [
     id: '1',
     name: 'silence',
     title: 'the realm the silence heard',
-    text: ['you are pacing instants, ', 'a rhythmic ethereal air, ', 'acute distraction draped upon, ', 'a far off dreamy layer. ', 'where eveything is happenstance, ', 'each face another place, ', 'take either way and let it pass, ', 'soon we call it fate. ', 'there\'s warmth in practice, ', 'taking time - to learn to contemplate, ', 'in the midst of mostly noise, ', 'subconscious congregates. ', 'the realm that birthed the universe, ', 'a realm that sees your words, ', 'in this collage, ', 'our senses wait, ', 'in the realm the silence heard. ']
+    text: ['you are pacing instants, ', 'a rhythmic ethereal air, ', 'acute distraction draped upon, ', 'a far off dreamy layer. ', 'where eveything is happenstance, ', 'each face another place, ', 'take either way and let it pass, ', 'soon we call it fate. ', 'there\'s warmth in practice, ', 'taking time - to learn to contemplate, ', 'in the midst of mostly noise, ', 'subconscious congregates. ', 'the realm that birthed the universe, ', 'the realm that sees your words, ', 'in this collage, ', 'our senses wait, ', 'in the realm the silence heard.']
   },
   {
     id: '2',
@@ -15,7 +15,7 @@ export const writings = [
     id: '3',
     name: 'earth',
     title: 'heaven on earth',
-    text: ['the static of the earth ', 'attracts the magnet of a mind. ', 'the fabric of the earth, ', 'informs the senses, ', 'of the lines. ', 'the will inside the earth, ', 'humbles each and every heir, ', 'the brilliance of the earth, ', 'arrives acutely, ', 'and can tear. ', 'the contents of the earth, ', 'include the nows that all have passed. ', 'the shadows on the earth, ', 'hide the light, ', 'that can\'t be grasped. ', 'the folks on top of earth, ', 'looking elsewhere for their souls, ', 'miss the part of earth, ', 'that harbors heaven and its holes.']
+    text: ['the static of the earth, ', 'attracts the magnet of a mind. ', 'the fabric of the earth, ', 'informs the senses, ', 'of the lines. ', 'the will inside the earth, ', 'humbles each and every heir, ', 'the brilliance of the earth, ', 'arrives acutely, ', 'and can tear. ', 'the contents of the earth, ', 'include the nows that all have passed. ', 'the shadows on the earth, ', 'hide the light, ', 'that can\'t be grasped. ', 'the folks on top of earth, ', 'looking elsewhere for their souls, ', 'miss the part of earth, ', 'that harbors heaven and its holes.']
   },
   {
     id: '4',
@@ -33,11 +33,18 @@ export const writings = [
     id: '6',
     name: 'water',
     title: 'water weight',
-    text: ['a tiny earthen crease, ', 'irrigation and release, ', 'precious drip, ', 'a tiny leak, ', 'walking water, ', 'wears all weak. ', 'a gymnast in a gentle stream, ', 'soon will swallow everything.']
+    text: ['a tiny earthen crease, ', 'irrigation and release, ', 'precious drip, ', 'a little leak, ', 'walking water, ', 'wears all weak. ', 'a gymnast in a gentle stream, ', 'soon will swallow everything.']
   },
   {
     id: 7,
     name: 'planet',
     title: 'planet earth',
     text: ['almost all water, ', 'and within every dew drop, ', 'brids punctuate the days.']
-  }];
+  },
+  {
+    id: 8,
+    name: 'tions',
+    title: 'tions',
+    text: ['while inside, ', 'the next mirror you find, ', 'do not think, ', 'about your sight, ', 'since the time, ', 'i thought of mine, ', 'mirrors won\'t tell me, ', 'what they find, ', 'though i look, ', 'and look and look, ', 'and turn around,', 'and play I\'m not,  ', 'surprise! i turn, ', 'around again, ', 'there\'s no surprise ', 'no mirror gives in, ', 'i see no thing, ', 'though standing still, ', 'i look so long, ', 'that wait! i must... ', 'i think i see... ', 'is that a bust?', 'is this thing real?, ', 'can I even trust? ', 'is this at last, ', 'a reflection!?, ', 'am i me? ', 'or my projection? ', 'and then it flickers, ', 'back to dust, ', 'it came and went ', 'and me nonplus ', 'no feels disturbed, ', 'no thoughtful hug, ', 'another me', 'will never be, ', 'go on i must ', 'yet to this day, ', 'me poached, my echo ', 'stays gone away.']
+  }
+];
