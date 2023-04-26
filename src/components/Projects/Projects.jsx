14b40e53d65d506bc72714faa949c3f5c84aa71c
adam robson <adamray312@gmail.com>
2023-04-26 07:03:56 -0700

    projects got a facelift

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,61 +1,84 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import buddystock from '../../assets/giphy.gif';
+import collabo from '../../assets/collabo.png';
+import tallyho from '../../assets/tallyho.png';
+import heart from '../../assets/heart-512.png';
+import marked from '../../assets/marked.png';
 
 export default function Projects() {
   return (
     <>
-      <section className="w-screen mx-auto my-20 h-screen">
-        <h2 className="text-center text-2xl md:text-3xl py-4 subpixel-antialiased underline w-2/3 mx-auto">projects</h2>
-        <div className="w-1/2 mx-auto text-center text-xs md:text-lg leading-loose subpixel-antialiased">
-          <p>All of the projects listed below were created by Adam Robson.  collabo and tallyho were built as a part of a team with 3-4 other developers.</p>  
-        </div>
-        <article className="flex flex-col items-center flex-wrap md:flex-row md:justify-evenly md:items-center mt-16 md:mx-16">
-          <Link
-            to="https://buddystockviii.com">
-            <img
-              src={ process.env.PUBLIC_URL + '/images/giphy.gif' }
-              alt="icon for buddystock"
-              className="max-h-44"
-            />
-            <p className="text-center mb-4 subpixel-antialiased">buddystock viii</p>
-          </Link>
-          <Link
-            to="https://collabomusic.co/">
-            <img
-              src={ process.env.PUBLIC_URL + '/images/collabo.png' }
-              alt="icon for collabo"
-              className="group/pic"
-            />
-            <p className="text-center mb-4 subpixel-antialiased">collabo</p>
-          </Link>
-          <Link
-            to="https://tally-ho-road-trips.netlify.app/auth/sign-in">
-            <img
-              src={ process.env.PUBLIC_URL + '/images/tallyho.png' }
-              alt="icon for tallyho"
-              className="group/pic"
-            />
-            <p className="text-center subpixel-antialiased">tallyho</p>
-          </Link>
-          <Link
-            to="https://https://deft-rolypoly-d4e615.netlify.app/">
-            <img
-              src={ process.env.PUBLIC_URL + '/images/book.png' }
-              alt="icon for good books"
-              className="group/pic"
-            />
-            <p className="text-center subpixel-antialiased">books</p>
-          </Link>
-          <Link
-            to="https://preeminent-bunny-fb20a4.netlify.app">
-            <img
-              src={ process.env.PUBLIC_URL + '/images/marked.png' }
-              alt="icon for marked-icon"
-              className="group/pic"
-            />
-            <p className="text-center subpixel-antialiased">markdown editor</p>
-          </Link>
-          <span>___</span>
+      <NavLink to="/" className="text-sm md:text-lg m-10 absolute top-0 left-0">home</NavLink>
+      <section className="">
+        <h2
+          className="subtitle max-w-max mx-auto text-center text-xl md:text-2xl my-16 p-2 subpixel-antialiased underline"
+        >
+          projects
+        </h2>
+        <aside className="caption text-center text-xs relative bottom-12"><em>*hover on image to view name</em></aside>
+       
+        <article className="text-center flex flex-col items-center justify-around">
+          <div className="my-10">
+            <Link
+              to="https://buddystockviii.com">
+              <img
+                src={ buddystock }
+                alt="icon for buddystock"
+                className="project_icon h-40"
+              />
+              <label className="label">buddystock viii</label>
+            </Link>
+          </div>
+          <div className="my-10">
+            <Link
+              to="https://collabomusic.co/">
+              <img
+                src={ collabo }
+                alt="icon for collabo"
+                className="project_icon"
+              />
+              <label className="label">collabo</label>
+            </Link>
+          </div>
+          <div className="my-10">
+            <Link
+              to="https://tally-ho-road-trips.netlify.app/auth/sign-in">
+              <img
+                src={ tallyho }
+                alt="icon for tallyho"
+                className="project_icon"
+              />
+              <label className="label">tallyho</label>
+            </Link>
+          </div>
+          <div className="my-10">
+            <Link
+              to="https://https://deft-rolypoly-d4e615.netlify.app/">
+              <img
+                src={ heart }
+                alt="icon for good books"
+                className="project_icon h-40"
+              />
+              <label className="label">books</label>
+            </Link>
+          </div>
+          <div className="my-10">
+            <Link
+              to="https://preeminent-bunny-fb20a4.netlify.app">
+              <img
+                src={ marked }
+                alt="icon for marked-icon"
+                className="project_icon"
+              />
+              <label className="label">markdown editor</label>
+            </Link>
+          </div>
+          <div className="caption w-1/2 mx-auto text-center text-sm md:text-lg subpixel-antialiased mb-20">
+            <em>
+              buddystock, books, and marxdown! were created by adam robson; tallyho and collabo were built by adam robson on a four-person team of developers.
+            </em>
+          </div>
         </article>
       </section>
     </>
