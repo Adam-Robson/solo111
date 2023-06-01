680ab7999af9081a38385300be34b5a90a145736
Adam Robson <adamray312@gmail.com>
2023-05-31 18:14:34 -0700

    functional download button for resume page

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,31 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import resume from '../../assets/resume.png';
 
 export default function Resume() {
-  const [downloadResume, setDownloadResume] = useState('');
-  
-  function downloadNewFile() {
-    const link = document.createElement('a');
-    const file = new Blob([downloadResume], { type: 'text/plain' });
-    link.href = URL.createObjectURL(file);
-    link.download = new Date() + 'Resume_Robson_Adam.pdf';
-    link.click();
-    URL.revokeObjectURL(link.href);
-  }
-
   return (
     <>
       <div className="animate__fadeIn min-h-screen w-screen flex flex-col items-center justify-start">
+      
         <img
           className="w-2/5 mx-auto md:mt-10"
           src={ resume }
           alt="resume"
         />
-        <button onClick={ () => downloadNewFile() } type="button" className="download">
+        <Link 
+          className="m-4" 
+          to="/resume.pdf" 
+          target="_blank" 
           download
-        </button>
-      </div>
+        >Download</Link>
 
+      </div>
     </>
   );
 }
