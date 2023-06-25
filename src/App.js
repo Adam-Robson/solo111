33913c97b0d09bf88b6c3d21f5e11b2d93a386b2
Adam Robson <adamray312@gmail.com>
2023-06-25 08:25:48 +0000

    update app js

--- src/App.js
+++ src/App.js
@@ -14,15 +14,13 @@ export default function App() {
   return (
     <>
       <Routes>
-        <Route index path='/' element={ <Home /> } />
+        <Route index path='/' element={ <Home /> } errorElement={ <NotFound/> } />
         <Route path='/bio' element={ <Bio /> } />
         <Route path='/projects' element={ <Projects /> } />
         <Route path='/resume' element={ <Resume /> } />
-        <Route path='/art' element={ <Art /> }>
-          <Route path="/art/:name" element={ <Writing /> } />
-        </Route>
+        <Route path='/art' element={ <Art /> } />
+        <Route path="/art/:name" element={ <Writing /> } />
         <Route path='/misc' element={ <Misc /> } />
-        <Route path='*' element={ <NotFound /> } />
       </Routes>
     </>
   );
