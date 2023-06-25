469750ed533427425f9d7e550cd1c3c7604de4cd
Adam Robson <adamray312@gmail.com>
2023-06-25 08:25:16 +0000

    updated all parts

--- src/components/Misc.jsx
+++ src/components/Misc.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import crystal from '../assets/crystal-min.png';
+import crystal from '../assets/crystal.png';
 
 export default function Misc() {
   return (
     <>
       <section className="w-screen h-screen fadein px-6 overflow-hidden">
-        <Link className="absolute right-4 top-4 text-xl subpixel-antialiased" to='/'>home</Link>
-        <h1 className="title text-8xl md:text-9xl relative top-8 subpixel-antialiased">misc.</h1>
+        <Link className="title text-8xl md:text-9xl relative top-8 subpixel-antialiased" to='/'>misc.</Link>
         <img src={crystal} className="h-96" alt="image of out my window"/>
         <aside>
           See some of my past adventures <Link to='https://lapaztravels.wordpress.com/'>here</Link>.
