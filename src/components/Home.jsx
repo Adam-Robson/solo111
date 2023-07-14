05e7dc0ffaeeebc4dc95d1b512b98cb87c2b503d
Adam Robson <adamray312@gmail.com>
2023-07-14 11:11:18 -0700

    fix the links for all screens, no longer folding behind the name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,23 +6,23 @@ export default function Home() {
     <>
       <main className="w-screen h-screen fadein">
         <div className="p-4">
-          <h1 className="title max-w-full m-2 text-7xl sm:text-8xl md:text-7xl subpixel-antialiased">this is a portfolio</h1>
+          <h1 className="title max-w-full m-2 text-5xl sm:text-6xl md:text-7xl subpixel-antialiased">this is a portfolio</h1>
         </div>
-        <div className="h-full flex flex-col absolute right-4 md:top-20 text-right">
+        <div className="h-full flex flex-col absolute right-4 sm:top-10 md:top-20 text-right">
           <NavLink
-            className="homelink text-3xl sm:text-4xl md:text-4xl my-6 sm:my-4 md:my-2 lg:my-4 subpixel-antialiased"
+            className="homelink text-2xl sm:text-3xl md:text-4xl my-2 lg:my-4 subpixel-antialiased"
             to="/bio"
           >bio</NavLink>
           <NavLink
-            className="homelink text-3xl sm:text-4xl md:text-4xl my-6 sm:my-4 md:my-2 lg:my-4 subpixel-antialiased"
+            className="homelink text-2xl sm:text-3xl md:text-4xl my-2 lg:my-4 subpixel-antialiased"
             to="/projects"
           >projects</NavLink>
           <NavLink
-            className="homelink text-3xl sm:text-4xl md:text-4xl my-6 sm:my-4 md:my-2 lg:my-4 subpixel-antialiased"
+            className="homelink text-2xl sm:text-3xl md:text-4xl my-2 lg:my-4 subpixel-antialiased"
             to="/arts"
           >arts</NavLink>
           <NavLink
-            className="homelink text-3xl sm:text-4xl md:text-4xl my-6 sm:my-4 md:my-2 lg:my-4 subpixel-antialiased"
+            className="homelink text-2xl sm:text-3xl md:text-4xl my-2 lg:my-4 subpixel-antialiased"
             to="/resume"
           >resume</NavLink>
         </div>
