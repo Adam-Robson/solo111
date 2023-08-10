236beb7e69a4d26e5694f639bf1050a5eee38ddf
Adam Robson <adamray312@gmail.com>
2023-08-10 16:23:41 -0700

    add detail to bio; change projects layout slightly

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -5,19 +5,19 @@ export default function Bio() {
     <>
       <section className="mx-auto fadein px-6 overflow-hidden">
         <Link className="text-xl absolute top-6 right-8 subpixel-antialiased" to="/">home</Link>
-        <h1 className="title text-8xl md:text-9xl relative top-12 subpixel-antialiased">bio</h1>
+        <h1 className="title text-8xl md:text-9xl relative top-12 subpixel-antialiased mb-2">bio</h1>
         <div className="md:grid md:grid-cols-2 md:gap-8 p-4">
           <div className="md:flex md:flex-col md:justify-evenly my-8">
-            <p className="title text-lg sm:text-2xl md:text-3xl lg:text-4xl text-right subpixel-antialiased">
+            <p className="title text-lg sm:text-2xl md:text-3xl lg:text-4xl text-right subpixel-antialiased my-2">
               I live in the pacific northwest.
             </p>
-            <p className="title text-lg sm:text-2xl md:text-3xl lg:text-4xl  text-right subpixel-antialiased">
+            <p className="title text-lg sm:text-2xl md:text-3xl lg:text-4xl  text-right subpixel-antialiased my-2">
               I am a musician & artist.
             </p>
-            <p className="title text-lg sm:text-2xl md:text-3xl lg:text-4xl text-right subpixel-antialiased">
-              I am a f/s software engineer.
+            <p className="title text-lg sm:text-2xl md:text-3xl lg:text-4xl text-right subpixel-antialiased my-2">
+              I became a f/s software engineer after a decade working as a legal assistant.
             </p>
-            <p className="title text-lg sm:text-2xl md:text-3xl lg:text-4xl text-right subpixel-antialiased">
+            <p className="title text-lg sm:text-2xl md:text-3xl lg:text-4xl text-right subpixel-antialiased my-2">
               I am proficient in postgresql, express, react, and node.
             </p>
           </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,60 +11,60 @@ export default function Projects() {
     <>
       <section className="mx-auto fadein px-6">
         <Link className="text-xl absolute top-6 right-8 subpixel-antialiased" to="/">home</Link>
-        <h1 className="title text-7xl md:text-8xl relative top-10 subpixel-antialiased">projects</h1>
+        <h1 className="title text-7xl md:text-8xl relative top-10 subpixel-antialiased mb-4">projects</h1>
         <div className="flex justify-center items-center">
-          <article className="container flex flex-col items-center xl:flex-row xl:flex-wrap xl:justify-evenly xl:items-center">
-            <div className="icon text-center text-3xl flex justify-center subpixel-antialiased p-6">
+          <article className="container flex flex-col items-center justify-centerres">
+            <div className="icon text-center text-3xl subpixel-antialiased mt-8">
               <Link
                 to="https://pantry-pal-colab.herokuapp.com/">
                 <img
                   src={ pantry }
                   alt="icon for pantry pal"
-                  className="project_icon w-72"
+                  className="project_icon max-w-40 max-h-40 w-40"
                 />
                 <label className="label text-center">pantry pal</label>
               </Link>
             </div>
-            <div className="text-center text-3xl subpixel-antialiased p-6 m-6">
+            <div className="text-center text-3xl subpixel-antialiased my-2">
               <Link
                 to="https://tourmaline-lollipop-044f88.netlify.app/">
                 <img
                   src={ playground }
                   alt="icon for playground"
-                  className="project_icon"
+                  className="project_icon max-w-40 max-h-40 w-40"
                 />
                 <label className="label">playground</label>
               </Link>
             </div>
-            <div className="text-center text-3xl subpixel-antialiased p-6 m-6">
+            <div className="text-center text-3xl subpixel-antialiased">
               <Link
                 to="https://buddystockviii.com">
                 <img
                   src={ BVIII }
                   alt="icon for buddystock"
-                  className="project_icon h-80 mb-10 md:mx-32"
+                  className="project_icon mb-10 md:mx-32 max-w-40 max-h-40 w-40"
                 />
                 <label className="label">buddystock</label>
               </Link>
             </div>
-            <div className="text-center text-3xl subpixel-antialiased p-6 m-6">
+            <div className="text-center text-3xl subpixel-antialiased">
               <Link
                 to="https://collabomusic.co/">
                 <img
                   src={ collabo }
                   alt="icon for collabo"
-                  className="project_icon h-80 my-4 md:mx-32"
+                  className="project_icon h-80 my-4 md:mx-32 max-w-40 max-h-40 w-40"
                 />
                 <label className="label">collabo</label>
               </Link>
             </div>
-            <div className="text-center text-3xl subpixel-antialiased p-6 m-6">
+            <div className="text-center text-3xl subpixel-antialiased my-2">
               <Link
                 to="https://adaamsfavoritebooks.netlify.app/">
                 <img 
                   src={ heart } 
                   alt="a heart image" 
-                  className="project_icon h-60 mb-10 md:mx-32" />
+                  className="project_icon mb-10 mx-auto max-w-40 max-h-40 w-40" />
                 <label className="label mb-12">favorite books</label>
               </Link>
             </div>
