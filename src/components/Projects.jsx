b275cb96126ee6249581b22f5f03d5a9d5fc0c1a
Adam Robson <adamray312@gmail.com>
2023-08-15 16:48:45 -0700

    fix icons for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,61 +11,62 @@ export default function Projects() {
     <>
       <section className="mx-auto fadein px-6">
         <Link className="text-xl absolute top-6 right-8 subpixel-antialiased" to="/">home</Link>
-        <h1 className="title text-7xl md:text-8xl relative top-10 subpixel-antialiased mb-4">projects</h1>
-        <div className="flex justify-center items-center">
-          <article className="container flex flex-col items-center justify-centerres">
-            <div className="icon text-center text-3xl subpixel-antialiased mt-8">
+        <h1 className="title text-7xl md:text-8xl relative top-10 subpixel-antialiased">projects</h1>
+        <div className="flex justify-center items-center my-20">
+          <article className="container flex flex-col items-center lg:flex-row lg:flex-wrap lg:justify-evenly lg:items-center">
+            <div className="text-3xl flex justify-center subpixel-antialiased p-6">
               <Link
                 to="https://pantry-pal-colab.herokuapp.com/">
                 <img
                   src={ pantry }
                   alt="icon for pantry pal"
-                  className="project_icon max-w-40 max-h-40 w-40"
+                  className="project_icon w-40"
                 />
                 <label className="label text-center">pantry pal</label>
               </Link>
             </div>
-            <div className="text-center text-3xl subpixel-antialiased my-2">
+            <div className="text-center text-3xl subpixel-antialiased p-6">
               <Link
                 to="https://tourmaline-lollipop-044f88.netlify.app/">
                 <img
                   src={ playground }
                   alt="icon for playground"
-                  className="project_icon max-w-40 max-h-40 w-40"
+                  className="project_icon w-40"
                 />
                 <label className="label">playground</label>
               </Link>
             </div>
-            <div className="text-center text-3xl subpixel-antialiased">
+            <div className="text-center text-3xl subpixel-antialiased p-6">
               <Link
                 to="https://buddystockviii.com">
                 <img
                   src={ BVIII }
                   alt="icon for buddystock"
-                  className="project_icon mb-10 md:mx-32 max-w-40 max-h-40 w-40"
+                  className="project_icon h-40"
                 />
                 <label className="label">buddystock</label>
               </Link>
             </div>
-            <div className="text-center text-3xl subpixel-antialiased">
+            <div className="text-center text-3xl subpixel-antialiased p-6">
               <Link
                 to="https://collabomusic.co/">
                 <img
                   src={ collabo }
                   alt="icon for collabo"
-                  className="project_icon h-80 my-4 md:mx-32 max-w-40 max-h-40 w-40"
+                  className="project_icon h-40"
                 />
                 <label className="label">collabo</label>
               </Link>
             </div>
-            <div className="text-center text-3xl subpixel-antialiased my-2">
+            <div className="text-3xl flex justify-center subpixel-antialiased p-6">
               <Link
                 to="https://adaamsfavoritebooks.netlify.app/">
-                <img 
-                  src={ heart } 
-                  alt="a heart image" 
-                  className="project_icon mb-10 mx-auto max-w-40 max-h-40 w-40" />
-                <label className="label mb-12">favorite books</label>
+                <img
+                  src={ heart }
+                  alt="icon for pantry pal"
+                  className="project_icon w-40"
+                />
+                <label className="label text-center">pantry pal</label>
               </Link>
             </div>
           </article>
