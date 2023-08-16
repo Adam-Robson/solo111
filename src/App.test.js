a43daa39eb6949e16101d3e1f02936d0ff455a26
Adam Robson <adamray312@gmail.com>
2023-08-15 18:00:29 -0700

    write tests for resume component

--- src/App.test.js
+++ src/App.test.js
@@ -7,58 +7,38 @@ test('renders heading on page', () => {
   expect(headingEl).toBeInTheDocument();
 });
 
-test('renders bio link on home page', () => {
+test('renders bio link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/bio/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkEl = screen.getByText(/bio/i);
+  expect(linkEl).toBeInTheDocument();
 });
 
-test('renders projects link on home page', () => {
+test('renders projects link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/projects/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkEl = screen.getByText(/projects/i);
+  expect(linkEl).toBeInTheDocument();
 });
 
-test('renders arts link on home page', () => {
+test('renders arts link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/arts/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkEl = screen.getByText(/arts/i);
+  expect(linkEl).toBeInTheDocument();
 });
 
-test('renders resume on home page', () => {
+test('renders resume link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/resume/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkEl = screen.getByText(/resume/i);
+  expect(linkEl).toBeInTheDocument();
+});
+
+test('renders misc link', () => {
+  render(<App />);
+  const linkEl = screen.getByText(/misc/i);
+  expect(linkEl).toBeInTheDocument();
 });
 
 test('renders author name', () => {
   render(<App />);
-  const textEl = screen.getByText(/Adam Robson/i);
-  expect(textEl).toBeInTheDocuemnt();
-});
-
-test('renders buy me a coffee icon link on home page', () => {
-  render(<App />);
-  const iconElement = screen.getByTestId('coffee-icon-link');
-  expect(iconElement).toBeInTheDocument();
-});
-
-test('renders github icon link on home page', () => {
-  render(<App />);
-  const iconElement = screen.getByTestId('github-icon-link');
-  expect(iconElement).toBeInTheDocument();
-
-});
-
-test('renders linkedin icon link on home page', () => {
-  render(<App />);
-  const iconElement = screen.getByTestId('linkedin-icon-link');
-  expect(iconElement).toBeInTheDocument();
-
-});
-
-test('renders spotify icon link home page', () => {
-  render(<App />);
-  const iconElement = screen.getByTestId('spotify-icon-link');
-  expect(iconElement).toBeInTheDocument();
+  const nameEl = screen.getByText(/adam robson/i);
+  expect(nameEl).toBeInTheDocument();
 });
