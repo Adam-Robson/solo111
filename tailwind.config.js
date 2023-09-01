298ce791de80bd098f5398e6151c234a6c707557
Adam Robson <adamray312@gmail.com>
2023-09-01 05:58:27 -0600

    initialize additional root files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/index.html',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
