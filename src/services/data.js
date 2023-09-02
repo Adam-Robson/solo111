d51e89d6bf052ebbf993522a1f18a082cf101f38
Adam Robson <adamray312@gmail.com>
2023-09-02 02:20:37 -0600

    work on projects

--- src/services/data.js
+++ src/services/data.js
@@ -26,9 +26,8 @@ export const writings = [
   }
 ];
 
-export const projects = {
-  "pantry pal": {
-    "description": `Pantry Pal is a mobile responsive web-application
+export const projects = [
+ `Pantry Pal is a mobile responsive web-application
                   for anyone seeking food assistance and was built with
                   React for the client-side and Flask for the Python
                   backend, MongoDB, which stores data gathered through
@@ -38,11 +37,8 @@ export const projects = {
                   GeoCoding API for converting addresses into geographic
                   coordinates. The project is deployed using Fly, Heroku,
                   and MongoAtlas to ensure smooth functionality and
-                  accessibility.`
-  },
-
-  'buddystock viii': {
-    "description": `Buddystock VIII is a mobile-responsive website for
+                  accessibility.`,
+   `Buddystock VIII is a mobile-responsive website for
                   an annual, non-profit, music festival held in Mosier,
                   Oregon, set against the backdrop of a cherry orchard
                   overlooking the Columbia Gorge. This project was built
@@ -51,10 +47,16 @@ export const projects = {
                   volunteers running logistics. The project was deployed
                   using cPanel and HostGator, ensuring the festival's
                   information was freely accessible online and on mobile
-                  devices.`
-  },
-  "tally ho": {
-    "description": `Tally-Ho! is a mobile responsive web-application
+                  devices.`, `Collabo is an innovative open-source song-project
+                  platform built as a solution for enable musicians
+                  to play music together remotely. The project was built
+                  with vanilla JavaScript, HTML and CSS, and centers
+                  around an audio mixer that incorporates the Waveform
+                  Playlist tool by Naomi Aro, which harnesses the power
+                  of the Web Audio API. Developed with a team of five
+                  developers, the platform is built using vanilla JavaScript,
+                  CSS, and HTML, and is accessible at collabomusic.co.`,
+                  `Tally-Ho! is a mobile responsive web-application
                   built with React and styled using Chakra UI. It
                   leverages various technologies to provide users with
                   a comprehensive platform for planning and organizing
@@ -66,22 +68,8 @@ export const projects = {
                   integration of Google Waypoints and JavaScript Maps API
                   provides users with the ability to plan and save road trip
                   routes effectively. Users can save their planned trips,
-                  enhancing the practicality and value of the platform.`
-  },
-  "collabo": {
-    "description": `Collabo is an innovative open-source song-project
-                  platform built as a solution for enable musicians
-                  to play music together remotely. The project was built
-                  with vanilla JavaScript, HTML and CSS, and centers
-                  around an audio mixer that incorporates the Waveform
-                  Playlist tool by Naomi Aro, which harnesses the power
-                  of the Web Audio API. Developed with a team of five
-                  developers, the platform is built using vanilla JavaScript,
-                  CSS, and HTML, and is accessible at collabomusic.co.`
-  },
-
-  "my favorite books": {
-    "description": `My Favorite Books is a personal project for
+                  enhancing the practicality and value of the platform.`,
+                  `My Favorite Books is a personal project for
                   cataloging a collection of favorite books.
                   The project emphasizes the use of HTML, CSS, JavaScript,
                   and Supabase to create a seamless experience for visitors.
@@ -92,10 +80,8 @@ export const projects = {
                   for styling and visual presentation.
                   The integration of Supabase as a
                   data source highlights integrates the use of APIs,
-                  dynamic data, and Postgres data storage.`
-  },
-  'playground': {
-    "description": `Playground is a hub of classic games, and consists
+                  dynamic data, and Postgres data storage.`,
+                 `Playground is a hub of classic games, and consists
                   of a landing page with a series of links to the various
                   game page.  the pages include including games such as
                   Shell Game and Block-Breaker. The collection is built
@@ -105,5 +91,4 @@ export const projects = {
                   engaging games, with scripts written by fellow
                   graduates, tutorial content from MDN, and my
                   own creations.`
-  }
-};
+];
