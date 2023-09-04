aba0d2b496fedeeff810680ccedab68046423c6c
Adam Robson <adamray312@gmail.com>
2023-09-04 11:33:02 -0600

    responsive images with overlay

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,44 +1,18 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
+import BioImage from './BioImage';
 import or from '../assets/slvrfls.png';
-import pool from '../assets/barcorner.png';
+import adam from '../assets/adaam.png';
 
 export default function Bio() {
   return (
-    <>
-      <section className="bg-section mx-auto fadein px-6 overflow-hidden">
-        <Link className="text-xl absolute top-10 right-10 subpixel-antialiased" to="/">home</Link>
-        <h1 className="text-6xl absolute top-20 left-10 subpixel-antialiased">bio</h1>
-
-
-        <label>
-          I am a JavaScript software developer and a musician living in the coastal forests of the northwest.
-
-        </label>
-        <img width="500" className="project hover:cursor-pointer relative top-48" src={or} alt="me at silver falls in the mist" />
-
-        <label>Working with software challenges creatively and analytically.</label>
-
-        <img width="500" className="project hover:cursor-pointer relative top-60" src={pool} alt="at a pool table in portland oregon" />
-        <h2>Fundamentals I try and live by:</h2>
-        <ul>
-          <li>
-            What I try to understand becomes integrated and is eventually a part of me. It has the power to transform.
-          </li>
-          <li>
-            Humility fosters growth and enriches interactions & experience.
-          </li>
-          <li>
-            The unique parts of individuals and the planet reinforce diversity as a wellspring of strength.
-          </li>
-          <li>
-            I try and acknowledge that I will go through mistakes and remember to consider them as an opportunity for growth.
-          </li>
-
-          <li>Collaboration and sharing is at the core of making meaning in the world.
-          
-          </li>
-        </ul>
-      </section>
-    </>
+    <section className="fadein px-6">
+      <Link className="text-xl absolute top-10 right-10 subpixel-antialiased" to="/">home</Link>
+      <h1 className="text-8xl subpixel-antialiased mt-12">bio</h1>
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+        <BioImage className=" max-w-md p-2 m-2" imageUrl={or} text="I am a software engineer, musician and artist living in the beautiful pacific northwest." />
+        <BioImage className=" max-w-md p-2 m-2" imageUrl={adam} text="I like working with a Postgres Express React and Node stack." />
+      </div>
+    </section>
   );
 }
