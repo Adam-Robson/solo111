06045b70ea923c0c789ed2f49f393417b282fa05
Adam Robson <adamray312@gmail.com>
2023-10-19 06:21:11 -0700

    add route components

--- app/components/TitleCard.tsx
+++ app/components/TitleCard.tsx
@@ -1,11 +1,11 @@
 
 export default function TitleCard() {
   return (
-    <div className="max-w-2xl absolute bottom-8 left-1/2 transform -translate-x-1/2 text-center">
+    <div className="max-w-2xl absolute bottom-12 left-1/2 transform -translate-x-1/2 text-center">
       <span className="titlewrap">
         <h2 className="title text-8xl font-light tracking-tight subpixel-antialiased">portfolio</h2>
       </span>
-        <p className="text-xl font-normal tracking-tight subpixel-antialiased">Adam Robson</p>
+        <p className="relative bottom-2 right-10 text-xl font-normal tracking-tight subpixel-antialiased">Adam Robson</p>
     </div>
   );
 }
