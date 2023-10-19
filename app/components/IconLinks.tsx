b726ee81a60bd8b43404ed3310db9fed9dc526d5
Adam Robson <adamray312@gmail.com>
2023-10-19 01:53:36 -0700

    add the navigation and icon links for contact to landing page

---
+++ app/components/IconLinks.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Link from 'next/link'
+import {
+  PiLinkedinLogoDuotone,
+  PiGithubLogoDuotone,
+  PiSpotifyLogoDuotone
+} from 'react-icons/pi';
+
+export default function IconLinks() {
+  return (
+    <ul className=" max-w-2xl w-full flex justify-between items-baseline">
+      <li>
+        <Link href="https://linkedin.com/in/adamrayrobson">
+          <PiLinkedinLogoDuotone className="h-8 w-8" />
+        </Link>
+      </li>
+      <li>
+        <Link href="https://github.com/Adam-Robson">
+          <PiGithubLogoDuotone className="h-8 w-8" />
+        </Link>
+      </li>
+      <li>
+        <Link href="https://spoti.fi/3QNqMuE">
+          <PiSpotifyLogoDuotone className="h-8 w-8" />
+        </Link>
+      </li>
+    </ul>
+  )
+}
---
+++ app/components/Nav.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Nav() {
+  return (
+    <nav className="flex flex-col">{[
+      ['bio', '/bio'],
+      ['projects', '/projects'],
+      ['resume', '/resume'],
+      ['contact', '/contact'],
+      ['music', '/music'],
+      ['writings', '/writings'],
+      ['misc.', '/misc']
+    ].map(([title, url]) => (
+      <Link 
+        key={url} 
+        href={url} 
+        className="rounded-lg px-3 pt-2 pb-1 text-sm font-normal tracking-tight subpixel-antialiased">
+          {title}
+      </Link>
+    ))}</nav>
+  )
+}
