597b52d99a8588d03ef01c3cb1a92ee5537f2f22
Adam Robson <adamray312@gmail.com>
2023-10-20 09:43:19 -0700

    add PDF component and ResumePage on resume route

--- app/components/PDF.tsx
+++ app/components/PDF.tsx
@@ -1,36 +1,18 @@
-import React from 'react';
+import React from 'react'
+import type { PDFTypeProps } from '../utils/data/types';
 import {
-  Page,
-  Text,
-  View,
   Document,
-  StyleSheet
-} from '@react-pdf/renderer';
+  Page
+} from 'react-pdf'
 
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: 'row',
-    backgroundColor: '#e4e4e4'
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1
-  }
-});
-
-export default function PDF() {
+const PDF: React.FC<PDFTypeProps> = ({ pdfUrl }) => {
   return (
-    <Document>
-      <Page size="A4" style={styles.page}>
-        <View style={styles.section}>
-          <Text>Section #1</Text>
-        </View>
-        <View style={styles.section}>
-          <Text>Section #2</Text>
-        </View>
-      </Page>
-    </Document>
+    <div className="h-full">
+      <Document file={pdfUrl}>
+        <Page />
+      </Document>
+    </div>
   )
 }
+
+export default PDF
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,9 +1,9 @@
-import PDF from "../components/PDF";
+import PDF from '../components/PDF'
 
 export default function ResumePage() {
   return (
-    <div className="max-w-full w-10/12 mx-auto fadein px-6">
-     <PDF />
+    <div>
+      <PDF />
     </div>
   );
 }
