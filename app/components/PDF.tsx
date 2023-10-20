bd0469212c57317a7a2bfd46f1b6b1192ac51df6
Adam Robson <adamray312@gmail.com>
2023-10-20 09:54:22 -0700

    update pdf with worker

--- app/components/PDF.tsx
+++ app/components/PDF.tsx
@@ -1,12 +1,19 @@
+'use client'
 import React from 'react'
 import type { PDFTypeProps } from '../utils/data/types';
 import {
   Document,
   Page
 } from 'react-pdf'
+import { pdfjs } from 'react-pdf';
 
-const PDF: React.FC<PDFTypeProps> = ({ pdfUrl }) => {
-  return (
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
+export default function PDF({ pdfUrl }: PDFTypeProps) {
+    return (
     <div className="h-full">
       <Document file={pdfUrl}>
         <Page />
@@ -14,5 +21,3 @@ const PDF: React.FC<PDFTypeProps> = ({ pdfUrl }) => {
     </div>
   )
 }
-
-export default PDF
