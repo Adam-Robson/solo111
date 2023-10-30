f19e4008d3c0b5b32b7759208323d8c5a2088f40
Adam Robson <adamray312@gmail.com>
2023-10-29 19:47:41 -0700

    add background image and customize tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const cairo = Cairo_Play({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Adam Robson | Catalog',
-  description: 'Built with Next, TypeScript, Tailwind, and Care'
+  description: 'Built with Next, TypeScript, & Tailwind'
 }
 
 export default function RootLayout({
