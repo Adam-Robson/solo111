991a1d5ee587b0490bdfbc04c9be4176cab87f3a
Adam Robson <adamray312@gmail.com>
2023-10-30 10:05:23 -0700

    add bio page

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function ResumePage() {
+export default function BioPage() {
   return (
     <div className="min-h-screen max-w-3xl w-11/12 mx-auto flex flex-col justify-center items-center p-24">
-      <h1>resume</h1>
+      <h1>bio</h1>
     </div>
   )
 }
