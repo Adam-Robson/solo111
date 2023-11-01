238777b34d6b5726836f2b79de482bd3a7e24d7c
Adam Robson <adamray312@gmail.com>
2023-11-01 09:23:37 -0700

    change gif and updat palettes

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,18 @@ export default function Home() {
   return (
     <>
       <Container>
-        <main
-          className="flex min-h-screen max-full mx-auto"
-          style={{
-            backgroundImage: `url('/images/sun.png');`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundSize: 'cover'
-          }}
-        >
-          <div
-            className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-          >
-            <TitleCard />
-          </div>
-          <div className="absolute bottom-6 left-3/4">
-            <SocialIconLinks />
+        <main className="min-h-full max-w-full">
+          <div className="full-size-background">
+            <div
+              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+            >
+              <TitleCard />
+            </div>
+
+            <div className="absolute bottom-6 left-3/4">
+              <SocialIconLinks />
+            </div>
+
           </div>
         </main>
       </Container>
