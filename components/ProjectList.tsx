0955591cb3740a77f9f5a364db9594373393fc02
Adam Robson <adamray312@gmail.com>
2023-11-03 19:15:29 -0700

    adjust padding on projects list

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -6,18 +6,18 @@ import type { ProjectType } from '@/lib/types'
 export default function ProjectList() {
   return (
     <div className="container">
-      <h1 className="text-center text-5xl subpixel-antialiased">
+      <h1 className="text-center text-7xl md:text-8xl font-light tracking-tighter subpixel-antialiased">
         Projects
       </h1>
-      <ul className="mt-10">{projects.map((project: ProjectType) => (
+      <ul className="mt-4">{projects.map((project: ProjectType) => (
         <li key={project.id}>
-          <div className="max-w-xl mx-auto h-40 p-12">
-            <h4 className="text-center text-xl/7">{project.title}</h4>
-            <p className="my-6">{project.description}</p>
-            <span className="flex justify-between items-baseline">
-              <Link href={project.githubLink}>GitHub</Link>
-              <Link href={project.liveLink}>Live Deploy</Link>
-            </span>
+          <div className="max-w-sm sm:max-w-md md:max-w-lg mx-auto h-32 sm:h-40 md:h-60 p-6 sm:p-10 md:p12">
+            <h4 className="text-center text-lg/5">{project.title}</h4>
+            <p className="my-2 text-xs/3">{project.description}</p>
+            <div className="max-w-xs flex justify-around items-baseline">
+              <Link className="text-sm md:text-lg" href={project.githubLink}>GitHub</Link>
+              <Link className="text-sm md:text-lg" href={project.liveLink}>Live Deploy</Link>
+            </div>
           </div>
         </li>
       ))}
--- lib/data.ts
+++ lib/data.ts
@@ -4,7 +4,7 @@ export const projects: ProjectType[] = [
   {
     id: 1,
     title: "Pantry Pal",
-    description: `Pantry Pal is a mobile responsive web-application that locates food pantries anywhere in the US.`,
+    description: `"Pantry Pal" is a mobile responsive web-application that locates food pantries anywhere in the U.S.`,
     githubLink: "https://github.com/Adam-Robson/colab19-6",
     liveLink: "https://pantry-pal-colab.herokuapp.com/",
   },
@@ -18,14 +18,14 @@ export const projects: ProjectType[] = [
   {
     id: 3,
     title: "Collabo",
-    description: `The "Open Source Song Project Platform" Collabo is an innovative collaborative platform that enables musicians from all corners of the globe to create music together remotely.`,
+    description: `"Collabo" is an innovative and collaborative open-source song project platform that enables musicians to remotely create music together.`,
     githubLink: "https://github.com/Collabo-Team/collabo",
     liveLink: "https://collabomusic.co/",
   },
   {
     id: 4,
     title: "Buddystock",
-    description: `Buddystock is an annual music festival held in Oregon.`,
+    description: `"Buddystock" is an annual music festival held in Oregon.`,
     githubLink: "https://github.com/Buddystock/buddy",
     liveLink: "https://www.buddystockviii.com",
   }
