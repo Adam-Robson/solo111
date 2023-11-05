52066f3ccf8d549b1aef800e0dc611f8c0d110b9
Adam Robson <adamray312@gmail.com>
2023-11-04 18:38:04 -0700

    add download link to the resume

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,7 @@
-import PDF from '@/components/PDF'
 import React from 'react'
+import Link from 'next/link'
+import PDF from '@/components/PDF'
+import { PiFloppyDiskDuotone } from 'react-icons/pi'
 
 export default function ResumePage() {
   return (
@@ -7,6 +9,16 @@ export default function ResumePage() {
       <h1 className="text-center text-7xl md:text-8xl font-light tracking-tighter subpixel-antialiased">
         resume
       </h1>
+      <div className="relative max-w-sm mx-auto my-10 flex justify-center items-baseline">
+        <Link
+          href="/pdf/resume.pdf"
+          download="resume.pdf"
+          className="flex justify-between items-center"
+        >
+          <PiFloppyDiskDuotone size={30} color={'#b39180'} />
+          Download
+        </Link>
+      </div>
       <div className="max-w-2xl">
         <PDF />
       </div>
