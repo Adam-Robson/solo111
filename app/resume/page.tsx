e17df08ac7d8a3eedd17f8830effecf799cf9c2a
Adam Robson <adamray312@gmail.com>
2023-11-14 10:10:12 -0800

    fix the download button style and change fonts on two routes

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import Link from 'next/link'
 import PDF from '@/components/PDF'
-import { PiFloppyDiskDuotone } from 'react-icons/pi'
 import DownloadButton from '@/components/DownloadButton'
+
 export default function ResumePage() {
   return (
     <div className="max-w-3xl mx-auto flex flex-col justify-center items-center mt-12">
@@ -12,9 +11,7 @@ export default function ResumePage() {
       <div className="relative max-w-sm mx-auto my-10 flex justify-center items-baseline">
         <DownloadButton />
       </div>
-      <div className="">
-        <PDF />
-      </div>
+      <PDF />
     </div>
   )
 }
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,19 +10,19 @@ export default function Projects({ projects }: ProjectListProps) {
         {projects.map((project) => (
           <li
             key={project.id}
-            className={`sm:p-10 md:p-12 m-4`}
+            className={`p-6 m-4`}
           >
             <div>
-              <div className='text-center text-xl/6 sm:text-2xl/7 md:text-3xl/8 subpixel-antialiased underline'>{project.title}</div>
+              <div className='text-center font-normal text-lg/6 sm:text-xl/7 md:text-2xl/8 subpixel-antialiased underline'>{project.title}</div>
             </div>
             <div className={`card shadow-sm rounded-md p-4 mt-2`}>
-              <div className="w-full p-4">
+              <div className="w-full px-4 pt-2 pb-6">
                 {project.description}
               </div>
 
               <div className="w-full flex justify-evenly items-baseline mx-auto">
-                <Link className="text-sm/4 md:text-lg/5" href={project.githubLink}>GitHub Repository</Link>
-                <Link className="text-sm/4 md:text-lg/5" href={project.liveLink}>Live Deploy</Link>
+                <Link className="proj-link text-sm/4 md:text-lg/5" href={project.githubLink}>GitHub Repository</Link>
+                <Link className="proj-link text-sm/4 md:text-lg/5" href={project.liveLink}>Live Deploy</Link>
               </div>
             </div>
           </li>
