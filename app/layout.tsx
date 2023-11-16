d876b4157ec615357dbbfeecff88bd3662ba778d
Adam Robson <adamray312@gmail.com>
2023-11-15 21:26:02 -0800

    add loader and pdf add body font and remove layout and loading components for indvidual pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
 import React, { Suspense } from 'react'
 import type { Metadata } from 'next'
-import { Cairo_Play } from 'next/font/google'
+import { Baloo_Da_2, Cairo_Play } from 'next/font/google'
 import Loader from '@/components/Loader'
 import Navigation from '@/components/Navigation'
 
 import './globals.css'
 
-const cairo = Cairo_Play({ subsets: ['latin'] })
+const cairo = Cairo_Play({ weight: "400", subsets: ['latin'] })
+const baloo = Baloo_Da_2({ weight: "400", subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: 'Adam Robson | Portfolio',
-  description: 'Home route for portfolio site by Adam Robson. Built with Next14, TypeScript, & Tailwind'
+  description: 'Portfolio by Adam Robson'
 }
 
 export default function RootLayout({
   children
 }: {
-    children: React.ReactNode
-  }) {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body className={cairo.className}>
+      <body className={`${cairo.className} ${baloo.className}`}>
         <Navigation />
         <Suspense fallback={<Loader />}>
           {children}
