ff63c585767d3535e0d0ecd8aa87a4f67ebf44af
Adam Robson <adamray312@gmail.com>
2023-11-15 19:34:14 -0800

    recompose the writings route to resemble the projects route for consistency

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,33 +1,29 @@
-"use client"
-import React, { useState } from 'react';
 import Link from 'next/link';
 import type { ProjectListProps } from '@/lib/types'
 
 export default function Projects({ projects }: ProjectListProps) {
   return (
-    <div className="hover:cursor-pointer">
-      <ul>
-        {projects.map((project) => (
-          <li
-            key={project.id}
-            className={`p-6 m-4`}
-          >
-            <div>
-              <div className='text-center font-normal text-lg/6 sm:text-xl/7 md:text-2xl/8 subpixel-antialiased underline'>{project.title}</div>
+    <ul>
+      {projects.map((project) => (
+        <li
+          key={project.id}
+          className={`p-6 m-4`}
+        >
+          <div>
+            <div className='text-center font-normal text-lg/6 sm:text-xl/7 md:text-2xl/8 subpixel-antialiased underline'>{project.title}</div>
+          </div>
+          <div className={`card shadow-sm rounded-md p-4 mt-2`}>
+            <div className="w-full px-4 pt-2 pb-6">
+              {project.description}
             </div>
-            <div className={`card shadow-sm rounded-md p-4 mt-2`}>
-              <div className="w-full px-4 pt-2 pb-6">
-                {project.description}
-              </div>
 
-              <div className="w-full flex justify-evenly items-baseline mx-auto">
-                <Link className="proj-link text-sm/4 md:text-lg/5" href={project.githubLink}>GitHub Repository</Link>
-                <Link className="proj-link text-sm/4 md:text-lg/5" href={project.liveLink}>Live Deploy</Link>
-              </div>
+            <div className="w-full flex justify-evenly items-baseline mx-auto">
+              <Link className="proj-link text-sm/4 md:text-lg/5" href={project.githubLink}>Go to Github</Link>
+              <Link className="proj-link text-sm/4 md:text-lg/5" href={project.liveLink}>See it Live</Link>
             </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
--- components/Writings.tsx
+++ components/Writings.tsx
@@ -1,47 +1,25 @@
-"use client"
-import React, { useState } from 'react';
 import type { WritingListProps } from '@/lib/types'
 
 export default function Writings({ writings }: WritingListProps) {
-  const [selectedWritingId, setSelectedWritingId] = useState<number | null>(null);
-  function handleHoveredWriting(writingId: number): void {
-    setSelectedWritingId(writingId);
-  }
-  function handleLeaveHoveredWriting(): void {
-    setSelectedWritingId(null);
-  }
+
   return (
-    <div className="max-w-2xl mx-auto p-4 hover:cursor-pointer">
-      <ul>
-        {
-          writings.map((writing) => {
-            const isHovered = selectedWritingId === writing.id;
+    <ul>
+      {writings.map((writing) => (
+        <li
+          key={writing.id}
+          className={`p-6 m-4`}
+        >
+          <div>
+            <div className='text-center font-normal text-lg/6 sm:text-xl/7 md:text-2xl/8 subpixel-antialiased underline'>{writing.title}</div>
+          </div>
+          <div className={`card shadow-sm rounded-md p-4 mt-2`}>
+            <div className="w-full px-4 pt-2 pb-6">
+              {writing.body}
+            </div>
 
-            return (
-              <li
-                key={writing.id}
-                className={isHovered ? 'hovered' : ''}
-                onMouseEnter={() => handleHoveredWriting(writing.id)}
-                onMouseLeave={handleLeaveHoveredWriting}
-              >
-                <section className="max-w-md sm:max-w-lg md:max-w-xl mx-auto min-h-min p-2 sm:p-4 md:p-6">
-                  <div className="text-center text-lg/5 md:text-xl/6 subpixel-antialiased">
-                    {writing.title}
-                  </div>
-                </section>
-
-                {isHovered && (
-                  <div className="max-w-sm mx-auto">
-                    <div className="w-full my-2 text-sm/4 md:text-lg/5 subpixel-antialiased">
-                      {writing.body}
-                    </div>
-                  </div>
-                )}
-              </li>
-            )
-          }
-          )}
-      </ul>
-    </div>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
