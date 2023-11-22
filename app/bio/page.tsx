1bad7fd74ed6da01045562e765897c483c4faff7
Adam Robson <adamray312@gmail.com>
2023-11-22 07:56:43 -0800

    minor fixes on the nav bar and bio page and projects per henry feedback

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -14,18 +14,18 @@ export default function BioPage() {
 
         <div className="div3 max-w-md w-full ml-8 pr-12">
           <p className='mb-4 text-sm md:text-lg font-normal tracking-tighter subpixel-antialiased'>
-            I am a full-stack software developer trained in <Link href="https://www.postgresql.org/"><b>Postgres</b></Link>, <Link href="https://expressjs.com/"><b>Express</b></Link>, <Link href="https://react.dev/"><b>React</b></Link>, <Link href="https://www.typescriptlang.org/"><b>TypeScript</b></Link>, and <Link href="https://nodejs.org/en"><b>Node</b></Link>.
+            As a dedicated full-stack developer, I thrive on perpetual learning, specializing in technologies such as <Link href="https://www.postgresql.org/"><b>Postgres</b></Link>, <Link href="https://expressjs.com/"><b>Express</b></Link>, <Link href="https://react.dev/"><b>React</b></Link>, <Link href="https://www.typescriptlang.org/"><b>TypeScript</b></Link>, and <Link href="https://nodejs.org/en"><b>Node</b></Link>.
           </p>
           <p className='mb-4 text-sm md:text-lg font-normal tracking-tighter subpixel-antialiased'>
-            After spending ten years in the legal field, I chose to transition to software.
+            With a decade-long background in the legal field, I made the pivotal decision to transition into the dynamic realm of software development.
           </p>
           <p className='mb-4 text-sm md:text-lg font-normal tracking-tighter subpixel-antialiased'>
-            In my personal time, I write, play, record, edit, and produce original music.
+            Beyond coding, my passion extends to writing, playing, recording, editing, and producing original music.
+          </p>
+          <p className='mb-4 text-sm md:text-lg font-normal tracking-tighter subpixel-antialiased'>
+            Explore my musical creations on <Link href="https://lefogsong.bandcamp.com/"><b>Bandcamp</b></Link> or search for 'le fog' on any major music platform.
           </p>
 
-          <p className='mb-4 text-sm md:text-lg font-normal tracking-tighter subpixel-antialiased'>
-            It can be found <Link href="https://lefogsong.bandcamp.com/"><b>here</b></Link> or by searching any major music platform for le fog.
-          </p>
 
         </div>
 
