09f23fa9194421c3622b4d4461901616aa19fc13
Adam Robson <adamray312@gmail.com>
2023-11-24 00:22:32 -0800

    add new resume and make minor changes in display

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image'
 
 export default function Loader() {
   return (
-    <div className="container">
+    <div className="container my-12 mx-auto">
       <Image
         src={`/gif/abstract.gif`}
-        width={`650`}
-        height={`650`}
+        width={`450`}
+        height={`450`}
         alt={`a gif to display while page loads`}
         className='rounded-md shadow-md'
       />
--- components/NavDrawer.tsx
+++ components/NavDrawer.tsx
@@ -20,19 +20,19 @@ export default function NavDrawer() {
   return (
     <>
       <NavButton onClick={toggleIsOpen} />
-      <div className={`${cairo.className} flex flex-col items-start justify-evenly absolute top-8 left-4 h-1/2 w-32 mt-6 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-1000 ease-in-out z-20`}>
+      <div className={`${cairo.className} flex flex-col items-start justify-evenly absolute top-8 left-4 h-2/5 w-32 mt-6 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-1000 ease-in-out z-20`}>
 
         <Link className="text-lg/4 sm:text-xl/5 md:text-2xl/6 tracking-tighter subpixel-antialiased" href="/" onClick={closeMenu}>
           home
         </Link>
-        <Link className="text-lg/4 sm:text-xl/5 md:text-2xl/6 tracking-tighter subpixel-antialiased" href="/projects" onClick={closeMenu}>
-          projects
+        <Link className="text-lg/4 sm:text-xl/5 md:text-2xl/6 tracking-tighter subpixel-antialiased" href="/bio" onClick={closeMenu}>
+          bio
         </Link>
         <Link className="text-lg/4 sm:text-xl/5 md:text-2xl/6 tracking-tighter subpixel-antialiased" href="/resume" onClick={closeMenu}>
           resume
         </Link>
-        <Link className="text-lg/4 sm:text-xl/5 md:text-2xl/6 tracking-tighter subpixel-antialiased" href="/bio" onClick={closeMenu}>
-          bio
+        <Link className="text-lg/4 sm:text-xl/5 md:text-2xl/6 tracking-tighter subpixel-antialiased" href="/projects" onClick={closeMenu}>
+          projects
         </Link>
         <Link className="text-lg/4 sm:text-xl/5 md:text-2xl/6 tracking-tighter subpixel-antialiased" href="/music" onClick={closeMenu}>
           music
