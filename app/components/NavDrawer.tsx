bee28e7a69085f3ce36e5d2be0e287f9885636a8
Adam Robson <adamray312@gmail.com>
2024-01-23 16:55:56 -0800

    adjust the nav drawer

--- app/components/NavDrawer.tsx
+++ app/components/NavDrawer.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Link from 'next/link';
 import React, { useState } from 'react'
-import { PiListDuotone } from 'react-icons/pi'
 import { Cairo_Play } from 'next/font/google';
 import NavButton from './NavButton';
 import DarkModeButton from './DarkModeButton';
@@ -21,7 +20,7 @@ export default function NavDrawer() {
   return (
     <>
       <NavButton onClick={toggleIsOpen} />
-      <div className={`${cairo.className} flex flex-col items-start justify-evenly absolute top-8 left-4 h-2/5 w-32 mt-6 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-1000 ease-in-out z-20`}>
+      <div className={`${cairo.className} flex flex-col items-start justify-around absolute top-10 left-4 h-3/5 w-32 mt-10 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-1000 ease-in-out z-20`}>
         
         <Link className="text-lg/4 sm:text-xl/5 md:text-2xl/6 tracking-tighter subpixel-antialiased" href="/" onClick={closeMenu}>
           home
