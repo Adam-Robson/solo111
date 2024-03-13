60b54325fe3adcc54c8fab97271474dbb62dda3e
Adam Robson <adamray312@gmail.com>
2024-03-12 18:37:43 -0700

    add screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import Loader from './components/Loader'
 
 export default function loading() {
   return (
-    <div className='min-h-full max-w-2xl mx-auto'>
+    <div className='min-h-full max-w-screen-xl mx-auto'>
       <Loader />
     </div>
   )
