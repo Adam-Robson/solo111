1cb8376026e75a3b5d02f1b3c3e4b4381fbfe5d9
Adam Robson <adamrayrobson@gmail.com>
2024-09-09 13:32:34 -0700

    round border on image on contact page and add padding
    Signed-off-by: Adam Robson <adamrayrobson@gmail.com>

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -4,26 +4,28 @@ export default function Contact() {
   return (
     <div className="h-full flex flex-col flex-1 items-center justify-center w-full mx-auto max-w-screen-sm">
 
+    <div className="w-full h-full p-6">
       <div className="relative w-full">
         <Image
           src="/gif/ctct.gif"
           alt="gif of the horizon"
-          width="350"
-          height="350"
+          width="300"
+          height="300"
           layout="responsive"
+          className="rounded-lg shadow-lg"
         />
 
-        <div className="absolute w-full h-full top-0 left-0 flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-black bg-opacity-50 text-white transition-opacity duration-300 ease-in-out text-xs sm:text-sm md:text-lg">
+        <article className="rounded-lg absolute w-full h-full top-0 left-0 flex flex-col items-center justify-center opacity-0 hover:opacity-100 bg-black bg-opacity-50 text-white transition-opacity duration-300 ease-in-out text-xs sm:text-sm md:text-lg">
           reach out through the links below or by email:
 
-          <div>
-            <address className="">adamrayrobson@gmail.com</address>
-          </div>
+          <span className='text-center'>
+            <address className="p-2">adamrayrobson@gmail.com</address>
+          </span>
 
-        </div>
+        </article>
 
       </div>
-
+      </div>
     </div>
   );
 }
