2a4cd9e48cd37f8df54dbc6ae3e97e8ba655bb50
Adam Robson <adamrayrobson@gmail.com>
2024-09-18 22:34:12 -0700

    add about page
    Signed-off-by: Adam Robson <adamrayrobson@gmail.com>

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Navigation from '../components/Navigation';
+import About from '../components/About';
+import SocialIcons from '../components/SocialIcons';
+
+export default function AboutPage() {
+  return (
+    <div className="h-screen max-w-screen-md mx-auto w-full flex flex-col justify-between items-center">
+      <Navigation />
+      <About />
+      <SocialIcons />
+    </div>
+  );
+}
